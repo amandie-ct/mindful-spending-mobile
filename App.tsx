e9341960ed0dc70cb4cd09fc595e3f251ad62904
Amanda Carvalho <carvalhotp.amanda@gmail.com>
2026-04-08 21:21:18 -0300

    feat: adding fonts

--- App.tsx
+++ App.tsx
@@ -1,44 +1,50 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import {
   SafeAreaProvider,
-  useSafeAreaInsets,
+  SafeAreaView,
 } from 'react-native-safe-area-context';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+import { Text } from './src/components/Text/Text';
 
+function App() {
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
+      <SafeAreaView style={styles.screen} edges={['top', 'right', 'bottom', 'left']}>
+        <Text
+          preset="headingLarge"
+          style={styles.title}
+        >
+          Mindful spending
+        </Text>
+        <Text
+          preset="headingSmall"
+          style={styles.body}
+        >
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni ex
+          asperiores, exercitationem amet fugiat explicabo ut vitae blanditiis
+          dolorum vero quidem, provident id, cumque obcaecati? Quia fugiat
+          vitae nihil officiis? Autem, molestiae placeat corrupti et recusandae
+          tempore praesentium, excepturi fugiat a accusamus, doloribus error
+          exercitationem sunt at numquam necessitatibus soluta? Ut, quis? Odit
+          quis minus praesentium architecto adipisci numquam veniam.
+        </Text>
+      </SafeAreaView>
     </SafeAreaProvider>
   );
 }
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
+    paddingHorizontal: 24,
+    backgroundColor: '#fff',
+  },
+  title: {
+    marginBottom: 20,
+    fontFamily: 'AbrilFatface-Regular',
+  },
+  body: {
+    fontFamily: 'Raleway-Medium-Original',
   },
 });
 
